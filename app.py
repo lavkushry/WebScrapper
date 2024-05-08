e10bbca70ae3a6395a9e165b977540fc8cef9a32
lavkushry <lavkushry@gmail.com>
2024-05-08 21:38:33 +0530

    Added all Imports

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import logging
+logging.basicConfig(filename='scrapper.log',level=logging.INFO)
 
 app=Flask(__name__)
 
@@ -7,4 +13,4 @@ def hello_world():
     return "Hello World!"
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=8080)
